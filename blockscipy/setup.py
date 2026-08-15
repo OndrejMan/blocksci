17ae7cb0f4a1ff14c8e523e3c37b1cfa030d4e5b
Ondrej Man <ondrejman1@gmail.com>
2026-08-15 11:06:50 +0200

    build: make compilation and notebooks configurable
    Honor NTHREADS and allow notebook setup to be skipped in lean builds.

--- blockscipy/setup.py
+++ blockscipy/setup.py
@@ -47,7 +47,7 @@ class CMakeBuild(build_ext):
             build_args += ['--', '/m']
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j4']
+            build_args += ['--', '-j{}'.format(os.environ.get('NTHREADS', '4'))]
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
